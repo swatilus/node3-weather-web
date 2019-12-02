02a9a2637bb50de91b5ffe12514c8383f6837287
Dan <swak13@gmail.com>
2019-12-02 20:14:13 +0700

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
-    const url = `http://localhost:3000/weather?address=${location}`
+    const url = `/weather?address=${location}`
 
     msgOne.textContent = 'Running...'
     msgTwo.textContent = ''
